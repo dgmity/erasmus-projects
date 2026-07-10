6c14d1d3ce50e6566b41bd75552b7b3879f1bcb5
dgmity <mksbrnk@gmail.com>
2026-07-10 11:35:15 +0200

    Add student destructing props

---
+++ react-js/src/Student.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Student({name, age, city}) {
+  return (
+    <div>
+        <h2>Name: {name}</h2>
+        <p>Age: {age}</p>
+        <p>City: {city}</p>
+    </div>
+  )
+}
+
+export default Student
