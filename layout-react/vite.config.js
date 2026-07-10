386dd60548abec2638f5b412b0f8abada99eadf7
dgmity <mksbrnk@gmail.com>
2026-07-10 14:46:27 +0200

    Changing base in vite config

--- layout-react/vite.config.js
+++ layout-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/erasmus-project/layout-site/',
+  base: '/erasmus-projects/layout-site/',
 })
