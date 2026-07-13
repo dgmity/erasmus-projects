a3ac60fd557b3e499a88e8987a3494c7ab5a9faa
dgmity <mksbrnk@gmail.com>
2026-07-13 11:44:39 +0200

    Filtem map exclude

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
   const students = ["Rahim", "Karim", "Sakib", "Hasan", "Hayeem", "Rafi"];
-  
-  const showStudents = ["Rahim", "Sakib"];
+
   return (
     <div>
-      {students.filter((student) => showStudents.includes(student)).map((student, index) => (
+      {students.filter((student) => student !== "Rahim" && student !== "Hasan").map((student, index) => (
         <h2 key={index}>{student}</h2>
       ))}
     </div>
