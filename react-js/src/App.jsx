ba7d82bc5969243ff637ef4d0bc328c70aed8ae5
dgmity <mksbrnk@gmail.com>
2026-07-14 11:29:59 +0200

    Name showing with states

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  const [liked, setLiked] = useState(false);
+  const [name, setName] = useState("");
   return (
     <div>
-      <button onClick={() => setLiked(!liked)}> {liked ? "Liked" : "Like"}</button>
+      <input placeholder="Enter name" onChange={(e) => setName(e.target.value)}></input>
+      <h2>Hello {name}</h2>
     </div>
   )
 }
