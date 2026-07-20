2c0c1ab7a63ec52d93ddc8858316bef58df519c5
dgmity <mksbrnk@gmail.com>
2026-07-20 10:54:45 +0200

    Weather app

--- react_portfolio/src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-function About(props) {
-  return (
-    <section id="about">
-      <p>My name is {props.name}. I am {props.age} guy from {props.city}. I graduated from {props.university} and now I am working as FullStack Developer in a huge corporation.</p>
-    </section>
-  )
-}
-
-export default About
